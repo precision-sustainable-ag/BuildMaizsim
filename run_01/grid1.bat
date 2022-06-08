bdeb8c2235154d44b699f012514892fc7d8268bb
ARS-CSGCL-DT <dennis.timlin@ars.usda.gov>
2022-06-08 13:01:23 -0400

    added missing run files

---
+++ run_01/grid1.bat
@@ -0,0 +1,6 @@
+D:\MAIZSIM07\CreateSoils\CreateSoilFiles_CO2.exe "D:\MAIZSIM07\AgMipEt2\run_01\run_01.lyr" /GN run_01 /SN MeadIr_run_01
+del output
+del element_elm
+del grid_bnd
+del datagen2.dat
+Dir  *.*  >dir.txt
