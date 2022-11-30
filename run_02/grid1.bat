aae94a36a602876a5bb0833dd9dddbc0286aecca
ARS-CSGCL-DT <dennis.timlin@ars.usda.gov>
2022-11-30 14:42:47 -0500

    added second example folder

---
+++ run_02/grid1.bat
@@ -0,0 +1,6 @@
+d:\maizsim07\CreateSoils\CreateSoilFiles_CO2.exe "D:\MAIZSIM07\AgMipEt2\run_02\run_02.lyr" /GN run_02 /SN MeadIr_run_02
+del output
+del element_elm
+del grid_bnd
+del datagen2.dat
+Dir  *.*  >dir.txt
