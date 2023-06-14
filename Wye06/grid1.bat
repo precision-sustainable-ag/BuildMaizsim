44c1587ac6b44c9e4b3cfc5b222d8d8db5f0ca2b
ARS-CSGCL-DT <dennis.timlin@ars.usda.gov>
2023-06-14 10:22:40 -0400

    Updated example from Wye, MD

---
+++ Wye06/grid1.bat
@@ -0,0 +1,6 @@
+D:\MAIZSIM07\CreateSoils\CreateSoilFiles.exe "D:\MAIZSIM07\MDEasternShore\Wye06\WYE06.lyr" /GN WYE06 /SN WyeSoil
+del output
+del element_elm
+del grid_bnd
+del datagen2.dat
+Dir  *.*  >dir.txt
